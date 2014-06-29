7bb5b19a41d6cc9ca61664a1676cee995546dbda
BiancaTud <bianca.tudora@gmail.com>
2014-06-29 03:04:37 +0300

    2 triangles of different colours and a square

--- Ubisoft/Ubisoft.cpp
+++ Ubisoft/Ubisoft.cpp
@@ -2,10 +2,18 @@
 //
 
 
+
 #include "stdafx.h"
 #include <GL/glew.h> // include GLEW and new version of GL on Windows
 #include <GLFW32/glfw3.h> // GLFW helper library
+#include <math.h>
+#include <iostream>
+
 // functie banala de incarcat continutul unui fisier intr-un buffer
+
+#define pi 3.14
+
+using namespace std;
 
 char * LoadFileInMemory(const char *filename)
 {
@@ -59,39 +67,117 @@ int main () {
 
   const char * vertex_shader = LoadFileInMemory("vertexShader.glsl");
   const char * fragment_shader = LoadFileInMemory("pixelShader.glsl");
+  const char * fragment_shader2 = LoadFileInMemory("pixelShader2.glsl");
 
   GLuint vs = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vs, 1, &vertex_shader, NULL);
   glCompileShader(vs);
+
   GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fs, 1, &fragment_shader, NULL);
   glCompileShader(fs);
+
+  GLuint fs2 = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs2, 1, &fragment_shader2, NULL);
+  glCompileShader(fs2);
+
   GLuint shader_programme = glCreateProgram();
   glAttachShader(shader_programme, fs);
   glAttachShader(shader_programme, vs);
   glLinkProgram(shader_programme);
 
+  GLuint shader_programme2 = glCreateProgram();
+  glAttachShader(shader_programme2, fs2);
+  glAttachShader(shader_programme2, vs);
+  glLinkProgram(shader_programme2);
+
+
   delete[] vertex_shader;
   delete[] fragment_shader;
 
   // buffer cu vertecsi in RAM 
-  float vertex_buffer[] = {
-	  0.0f, 0.5f, 0.0f,
-	  0.5f, -0.5f, 0.0f,
-	  -0.5f, -0.5f, 0.0f
+  float vertex_buffer_triangle1[] = {
+	  -0.5f, 0.7f, 0.0f,
+	  -0.3f, 0.3f, 0.0f,
+	  -0.7f, 0.3f, 0.0f
   };
 
-  // Generam un buffer in memoria video si scriem in el punctele din ram
-  GLuint vbo = 0;
-  glGenBuffers(1, &vbo); // generam un buffer 
-  glBindBuffer(GL_ARRAY_BUFFER, vbo); // setam bufferul generat ca bufferul curent 
-  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof (float), vertex_buffer, GL_STATIC_DRAW); //  scriem in bufferul din memoria video informatia din bufferul din memoria RAM
+  //triangle 2
+   float vertex_buffer_triangle2[] = {
+	  0.6f, 0.7f, 0.0f,
+	  0.4f, 0.3f, 0.0f,
+	  0.8f, 0.3f, 0.0f
+  };
 
-  // De partea aceasta am uitat sa va spun la curs -> Pentru a defini bufferul alocat de opengl ca fiind buffer de in de atribute, stream de vertecsi trebuie sa :
-  // 1. Ii spunem OpenGL-ului ca vom avea un slot pentru acest atribut (in cazul nostru 0) , daca mai aveam vreun atribut ar fi trebuit si acela enablat pe alt slot (de exemplu 1)
-  // 2. Definit bufferul ca Vertex Attribute Pointer cu glVertexAttribPointer
+   //square
+  float vertex_buffer_square[] = {
+	 -0.2f, -0.2f, 0.0f,
+	  0.2f, -0.2f, 0.0f,
+	  0.2f, 0.2f, 0.0f,
+	 -0.2f, -0.2f, 0.0f,
+	  0.2f, 0.2f, 0.0f,
+	 -0.2f, 0.2f, 0.0f
+  };
+
+ /* 
+  int nr;
+
+  nr=20;
+
+ double vertex_buffer_circle[1000];
+
+  int i;
+
+  double unghi = 2*pi/nr;
+
+  cout<<"unghi"<<unghi;
+
+  for(i=0;i<nr*3;i+=3){
+	  vertex_buffer_circle[i]=0.2*cos(unghi*(i/3));
+	  vertex_buffer_circle[i+1]=0.2*sin(unghi*(i/3));
+	  vertex_buffer_circle[i+2]=0;
+	  cout<<i<<endl;
+	  cout<<vertex_buffer_circle[i]<<endl;
+	  cout<<vertex_buffer_circle[i+1]<<endl;
+  }
+
+  cout<<endl;
+  cout<<"cos"<<cos(pi)<<endl;
+  cout<<"sin"<<sin(pi);
+
+*/
+  GLuint vbo[3], vao[3];  
+
+  glGenVertexArrays(3, &vao[0]);
+  // Generam un buffer in memoria video si scriem in el punctele din ram
+  
+  //triangle 1
+  vbo[0] = 0;
+  glBindVertexArray(vao[0]);
+  glGenBuffers(1, &vbo[0]); // generam un buffer 
+  glBindBuffer(GL_ARRAY_BUFFER, vbo[0]); // setam bufferul generat ca bufferul curent 
+  glBufferData(GL_ARRAY_BUFFER,  sizeof (vertex_buffer_triangle1), vertex_buffer_triangle1, GL_STATIC_DRAW); //  scriem in bufferul din memoria video informatia din bufferul din memoria RAM
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  //triangle 2
+  vbo[1] = 0;
+  glBindVertexArray(vao[1]);
+  glGenBuffers(1, &vbo[1]); // generam un buffer 
+  glBindBuffer(GL_ARRAY_BUFFER, vbo[1]); // setam bufferul generat ca bufferul curent 
+  glBufferData(GL_ARRAY_BUFFER,  sizeof (vertex_buffer_triangle2), vertex_buffer_triangle2, GL_STATIC_DRAW); 
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  //square 
+  vbo[2] = 0;
+  glBindVertexArray(vao[2]);
+  glGenBuffers(1, &vbo[2]); // generam un buffer 
+  glBindBuffer(GL_ARRAY_BUFFER, vbo[2]); // setam bufferul generat ca bufferul curent 
+  glBufferData(GL_ARRAY_BUFFER,  sizeof (vertex_buffer_square), vertex_buffer_square, GL_STATIC_DRAW); 
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
 
   while (!glfwWindowShouldClose(window)) {
 	  //..... Randare................. 
@@ -100,15 +186,26 @@ int main () {
 	  // spunem ce shader vom folosi pentru desenare
 	  glUseProgram(shader_programme);
 	  // facem bind la vertex buffer
-	  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	  glBindVertexArray(vao[0]);
 	  // draw points 0-3 from the currently bound VAO with current in-use shader
 	  glDrawArrays(GL_TRIANGLES, 0, 3);
-	  // facem swap la buffere (Double buffer)
+
+	  //triangle 2
+	  glUseProgram(shader_programme2);
+	  glBindVertexArray(vao[1]);
+	  glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	  //square
+	  glBindVertexArray(vao[2]);
+	  glDrawArrays(GL_TRIANGLES, 0,6);
+
+	 //glDrawArrays(GL_TRIANGLES, 0, nr);
+	 // facem swap la buffere (Double buffer)
 	  glfwSwapBuffers(window);
 
 	  glfwPollEvents();
 	}
-  
+
   glfwTerminate();
   return 0;
 }
